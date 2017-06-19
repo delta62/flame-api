fde73d2da14f5b7546acaae158a7d6c6f15c4999
Sam Noedel <sam.noedel@gmail.com>
2017-06-19 10:28:51 -0700

    Remove ES6 features for node4 compat

--- test/hello-tests.js
+++ test/hello-tests.js
@@ -1,4 +1,6 @@
-const { describe, it } = require('mocha')
+const mocha  = require('mocha'),
+    describe = mocha.describe,
+    it       = mocha.it
 
 describe('the app', () => {
   it('should not blow up', () => true)
