93e72d2441794dd53e064f47f3fd66b2d1884ba5
Sam Noedel <sam.noedel@gmail.com>
2017-06-19 10:35:16 -0700

    Fix linter issues

--- test/hello-tests.js
+++ test/hello-tests.js
@@ -1,6 +1,6 @@
 const mocha  = require('mocha'),
-    describe = mocha.describe,
-    it       = mocha.it
+  describe = mocha.describe,
+  it       = mocha.it
 
 describe('the app', () => {
   it('should not blow up', () => true)
