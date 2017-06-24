0037c628c420b4cc118f5a812b169340f56eff87
Sam Noedel <sam.noedel@gmail.com>
2017-06-24 09:17:41 -0700

    Simplifiy promise generation function

--- src/promise.js
+++ src/promise.js
@@ -1,9 +1,6 @@
 Promise.fromNodeCallback = function(cb, ...args) {
   return new Promise((resolve, reject) => {
-    args.push((err, data) => {
-      if (err) reject(err)
-      else resolve(data)
-    })
+    args.push((err, data) => err ? reject(err) : resolve(data))
     cb.apply(cb, args)
   })
 }
