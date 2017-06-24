2138287679f7507291ad1e042c561f36118f5bcd
Sam Noedel <sam.noedel@gmail.com>
2017-06-24 13:05:12 -0700

    Reorganize top-level server logic

--- src/server.js
+++ src/server.js
@@ -1,44 +1,44 @@
-const pkg                            = require('../package.json')
-const config                         = require('config')
 const { createServer, bodyParser }   = require('restify')
-const { createLogger }               = require('bunyan')
 const { reqStartTimer, reqEndTimer } = require('./middleware/timer')
-const { timestampFactory }           = require('./timestamp')
-const { Lock }                       = require('./repositories/lock')
 const { statusGetHandler }           = require('./handlers/status')
+const { channelPostHandler }         = require('./handlers/channel')
 const {
   lockGetHandler,
   lockPostHandler
 } = require('./handlers/lock')
 
-const logger = createLogger({ name: pkg.name })
-const server = createServer({
-  name: pkg.name,
-  log:  logger
-})
+function mkServer({ name, logger, lock, channels }) {
+  const server = createServer({
+    name,
+    log: logger
+  })
 
-server.lock = new Lock(config.get('lockTimeout') * 1000, timestampFactory)
+  Object.assign(server, lock, channels)
 
-server.use(bodyParser({ mapParams: false }))
-server.use(reqStartTimer)
+  server.use(bodyParser({ mapParams: false }))
+  server.use(reqStartTimer)
 
-server.get('/status',       statusGetHandler)
-server.get('/burner/lock',  lockGetHandler)
-server.post('/burner/lock', lockPostHandler)
+  server.get('/status',                   statusGetHandler)
+  server.get('/burner/lock',              lockGetHandler)
+  server.post('/burner/lock',             lockPostHandler)
+  server.post('/burner/channel/:channel', channelPostHandler)
 
-server.on('after', (req, res) => {
-  reqEndTimer(req)
-  server.log.info({
-    status:  res.statusCode,
-    method:  req.method,
-    path:    req.path(),
-    resTime: req.time
-  }, 'after')
-})
+  server.on('after', (req, res) => {
+    reqEndTimer(req)
+    server.log.info({
+      status:  res.statusCode,
+      method:  req.method,
+      path:    req.path(),
+      resTime: req.time
+    }, 'after')
+  })
 
-server.on('uncaughtException', (req, res, route, err) => {
-  err && err instanceof Error && server.log.error(err)
-  res.send(err)
-})
+  server.on('uncaughtException', (req, res, route, err) => {
+    err && err instanceof Error && server.log.error(err)
+    res.send(err)
+  })
 
-module.exports = { server }
+  return server
+}
+
+module.exports = { mkServer }
