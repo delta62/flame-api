e52f00106ae2af9383619533cd87920f8cf24e15
Sam Noedel <sam.noedel@gmail.com>
2017-06-25 11:21:35 -0700

    Log detailed error messages

--- src/server.js
+++ src/server.js
@@ -26,14 +26,22 @@ function mkServer({ name, logger, lock, channels }) {
   server.post('/burner/lock',             lockPostHandler)
   server.post('/burner/channel/:channel', channelPostHandler)
 
-  server.on('after', (req, res) => {
+  server.on('after', (req, res, route, err) => {
     reqEndTimer(req)
-    server.log.info({
+    const logData = {
       status:  res.statusCode,
       method:  req.method,
       path:    req.path(),
-      resTime: req.time
-    }, 'after')
+      resTime: req.reqTime
+    }
+
+    let logLevel = 'info'
+    if (err && err instanceof Error) {
+      logLevel = 'error'
+      logData.err = err
+    }
+
+    server.log[logLevel](logData, 'end')
   })
 
   server.on('uncaughtException', (req, res, route, err) => {
