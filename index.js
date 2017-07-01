43ea969ecdf20ec952e0a1351abc62c54eadb2d2
Sam Noedel <sam.noedel@gmail.com>
2017-06-30 20:09:49 -0700

    Add more detailed startup logging

--- index.js
+++ index.js
@@ -18,8 +18,18 @@ const lock     = new Lock(config.get('lockTimeout') * 1000, timestampFactory)
 const channels = initChannels(gpio, config.get('channels'), timestampFactory)
 const server   = mkServer({ name, logger, lock, channels })
 
+Object.keys(server.router.routes).map(key => {
+  server.router.routes[key].forEach(route => {
+    server.log.info({
+      method: route.spec.method,
+      path: route.spec.path
+    }, 'addRoute')
+  })
+})
+
 server.listen(port, () => {
   logger.info(`${server.name} listening on ${server.url}`)
+  logger.info('Now serving propane... and propane accessories')
 })
 
 setInterval(() => resetChannels(channels, timeout), 1000)
